4754e0f57b182f6c5b6f23bc99a7cd10b8c44110
HikiMan <tech.denisov@gmail.com>
2023-07-17 17:38:42 +0400

    feat: add register form

--- js/popup.js
+++ js/popup.js
@@ -1,13 +1,42 @@
-let popUpLogin = document.querySelector('.popup');
-let openPopUpButton = document.querySelector('.button-login');
-let background = document.querySelector('.popup-container');
-
+const popUpLogin = document.querySelector('.popup');
+const openPopUpButton = document.querySelector('.button-login');
+const background = document.querySelector('.popup-container');
+const background2 = document.querySelector('.popup2-container');
+const popUpRegister = document.querySelector('.popup2');
+const openPopUpRegisterLink = document.querySelector('.register-link');
+ 
 openPopUpButton.addEventListener('click', () => {
-    popUpLogin.classList.add('open');
+popUpLogin.classList.add('open');
+
+})
+
+openPopUpRegisterLink.addEventListener('click', () => {
+    popUpLogin.classList.remove('open');
+    
+})
+openPopUpRegisterLink.addEventListener('click', () => {
+    popUpRegister.classList.add('open');
+    
 })
 
 document.addEventListener('click', (e) => {
-    if (e.target === background) {
+    if (e.target == background) {
         popUpLogin.classList.remove('open');
+        popUpRegister.classList.remove('open');
     }
 })
+
+document.addEventListener('click', (e) => {
+    if (e.target == background2) {
+        popUpLogin.classList.remove('open');
+        popUpRegister.classList.remove('open');
+    }
+})
+
+
+
+
+
+
+
+
